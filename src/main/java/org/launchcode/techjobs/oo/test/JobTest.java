6d327acd8523e86935bb0723ae984fce016d995f
Karina Lage <karinalage@karinas-air.lan>
2022-09-18 18:42:21 -0500

    Completed step 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,48 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+    @Test
+    public void testSettingJobId() {
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertNotEquals(testJob1.getId(), testJob2.getId(), .001);
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertTrue(testJob3.getName() instanceof String);
+        assertTrue(testJob3.getEmployer() instanceof Employer);
+        assertTrue(testJob3.getLocation() instanceof Location);
+        assertTrue(testJob3.getPositionType() instanceof PositionType);
+        assertTrue(testJob3.getCoreCompetency() instanceof CoreCompetency);
+        assertEquals("Product tester", testJob3.getName());
+        assertEquals("ACME", testJob3.getEmployer().getValue());
+        assertEquals("Desert", testJob3.getLocation().getValue());
+        assertEquals("Quality control", testJob3.getPositionType().getValue());
+        assertEquals("Persistence", testJob3.getCoreCompetency().getValue());
+
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job testJob4 = new Job("Software Developer", new Employer("LaunchCode"), new Location("Saint Louis"), new PositionType("Unit Testing"), new CoreCompetency("JUnit"));
+        Job testJob5 = new Job("Software Developer", new Employer("LaunchCode"), new Location("Saint Louis"), new PositionType("Unit Testing"), new CoreCompetency("JUnit"));
+        assertFalse(testJob4.equals(testJob5));
+
+    }
 
 }
